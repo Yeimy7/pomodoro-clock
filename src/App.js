9c371c1c27fe1f6eda38964da1f3f6f9d7f735e8
Yeim <brielalica@gmail.com>
2020-09-28 16:01:29 -0400

    ya casi listo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,57 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [session, setSession] = useState(25);
+  const [breaking, setBreaking] = useState(5);
+
+  let min = session;
+  let s = 10;
+  let sw = true;
+  let inter;
+
+  const handleClock = () => {
+    let clock = document.getElementById('clock');
+    inter = setInterval(() => {
+      if (s === 0) {
+        s = 10;
+        if (min === 0) {
+          min = sw ? breaking : session;
+          sw = !sw;
+        } else {
+          min--;
+        }
+      }
+      s--;
+      let m = min < 10 ? '0' + min : min;
+      let ss = s < 10 ? '0' + s : s;
+      clock.innerText = m + ':' + ss;
+    }, 1000);
+  }
+  const Restart = () => {
+    let clock = document.getElementById('clock');
+    clearInterval(inter);
+    setSession(25);
+    setBreaking(5);
+    clock.innerText = session + ':00';
+  }
+
   return (
     <div>
       <h2>Session Length</h2>
-      <Session />
+      <Session session={session} setSession={setSession} />
       <h2>Break Length</h2>
+      <Breaking breaking={breaking} setBreaking={setBreaking} />
+      <div id='clock'>
+        {(sw) ? session + ':00' : breaking + ':00'}
+      </div>
+      <div>
+        <button onClick={handleClock}>Play</button>
+        <button onClick={Restart}>Restart</button>
+      </div>
     </div>
   )
 }
-const Session = () => {
-  const [session, setSession] = useState(25);
+const Session = ({ session, setSession }) => {
 
   const handleIncrement = () => {
     session < 60 && setSession(session + 1);
@@ -27,6 +68,23 @@ const Session = () => {
     </>
   )
 }
+
+const Breaking = ({ breaking, setBreaking }) => {
+  const handleIncrement = () => {
+    breaking < 60 && setBreaking(breaking + 1);
+  }
+  const handleDecrement = () => {
+    breaking > 1 && setBreaking(breaking - 1);
+  }
+  return (
+    <>
+      <button onClick={handleIncrement}>+</button>
+      <p>{breaking}</p>
+      <button onClick={handleDecrement}>-</button>
+    </>
+  )
+}
+
 
 
 export default App;
