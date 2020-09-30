40ac040ea0343e2a6696d151b9da2a01c29631e0
Yeim <brielalica@gmail.com>
2020-09-30 17:36:13 -0400

    docs creado

---
+++ docs/precache-manifest.656a8a1549f02d7c371bade8944dc3e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b40353042a4dece9243c34629303ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "560b689530cff55c49ee",
+    "url": "/static/css/main.5f5c3145.chunk.css"
+  },
+  {
+    "revision": "3bdec051e0b7fca697a4",
+    "url": "/static/js/2.e0b2333e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0b2333e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "560b689530cff55c49ee",
+    "url": "/static/js/main.3b5e8a38.chunk.js"
+  },
+  {
+    "revision": "5545b35b405a15ad83d4",
+    "url": "/static/js/runtime-main.7b910111.js"
+  }
+]);
